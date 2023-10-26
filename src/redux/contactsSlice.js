43147f4fded57fe4af2560f145e698b224a2bf21
ndnastia <ndnastiadatsiuk@gmail.com>
2023-10-26 17:31:28 +0300

    fixed state

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,12 +3,14 @@ import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
 
 const contactInitialState = {
-    contacts: [
+    contacts:[
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]};
+]
+}
+     ;
 
 const contactDetailsSlice = createSlice({
     name: 'contactDetails',
@@ -23,7 +25,7 @@ const contactDetailsSlice = createSlice({
     }
 });
 
-export const getContact = state => state.contactDetails;
+export const getContact = state => state.contact.contacts;
 
 export const { addContact, deleteContact } = contactDetailsSlice.actions;
 
